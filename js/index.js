61c1fc6592be88790f6d5d3fad03d7d00a886fc7
MusawarShahSyed6969 <97845228+MusawarShahSyed6969@users.noreply.github.com>
2022-02-01 02:01:20 -0800

    added secret button

---
+++ js/index.js
@@ -0,0 +1,11 @@
+  function lovecalculator() {
+    alert("You Open Love Calculator Pleace Type Your Name and Your Crush")
+    prompt("Your Name");
+    prompt("Your Crush Name");
+
+    let r = Math.random();
+    r = r * 100;
+    r = Math.floor(r) + 1;
+
+    alert(r + "%");
+}
